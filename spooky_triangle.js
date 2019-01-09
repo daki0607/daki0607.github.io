2147c65e8dff30a97ed80b963b34f84d10148415
Aditya <adityadangra@gmail.com>
2019-01-08 19:55:20 -0800

    Changed for TSpray
    Changed back to the spooky triangle demo because TSpray asked for it.

---
+++ spooky_triangle.js
@@ -0,0 +1,112 @@
+// Visual representation of fractals from chaos.
+
+let pos;
+let verticies = [];
+let chosen = false;
+let clicks = 5;
+let numVerticies = 3;
+let clock;
+let threshold = 1000;
+
+function draw_verticies(verticies) {
+  translate(width/2, height/2);
+  scale(1, -1);
+  noStroke();
+  fill(color(50, 200, 50, 127));
+
+  for (let i = 0; i < numVerticies; i++) {
+    ellipse(verticies[i].x, verticies[i].y, 15, 15);
+  }
+}
+
+function next_point(choice, verticies, size) {
+  let newX = (pos.x + verticies[choice].x) / 2;
+  let newY = (pos.y + verticies[choice].y) / 2;
+  pos.set(newX, newY);
+
+  stroke(255, 255, 255, 10);
+
+  if (size > 0) {
+    ellipse(pos.x, pos.y, size, size);
+  }
+
+  else {
+    point(pos.x, pos.y);
+  }
+}
+
+function setup() {
+  createCanvas(min(windowWidth, windowHeight), min(windowWidth, windowHeight));
+  background(51);
+
+  let scalingFactor = width / 2 * 0.85;
+  let angleBetween = TWO_PI / numVerticies;
+  let offset = (numVerticies % 2 == 0) ? angleBetween / 2 : 0;
+
+  for (let i = 0; i < numVerticies; i++) {
+    let x = scalingFactor * cos(offset + PI/2 + angleBetween * i);
+    let y = scalingFactor * sin(offset + PI/2 + angleBetween * i);
+
+    verticies.push(createVector(x, y));
+  }
+
+  draw_verticies(verticies);
+}
+
+function draw() {
+  translate(width/2, height/2);
+  scale(1, -1);
+
+  let currentTime = millis();
+  
+  if (currentTime - clock > threshold && threshold > 0) {
+    clock = currentTime;
+    threshold -= 65;
+
+    let choice = floor(random(numVerticies));
+
+    if (chosen) {
+      next_point(choice, verticies, 2);
+    }
+  }
+
+  if (threshold <= 0) {
+    for (let i = 0; i < 100; i++) {
+      let choice = floor(random(numVerticies));
+
+      if (chosen) {
+        next_point(choice, verticies, 0);
+      }
+    }
+  }
+}
+
+function mouseClicked() {
+  if (chosen && clicks > 0) {
+    let choice = floor(random(numVerticies));
+
+    let newX = (pos.x + verticies[choice].x) / 2;
+    let newY = (pos.y + verticies[choice].y) / 2;
+    pos.set(newX, newY);
+
+    noStroke();
+    fill(255);
+    ellipse(pos.x, pos.y, 2 + clicks/5, 2 + clicks/5);
+    clicks--;
+
+    if (clicks == 0) {
+      clock = millis();
+    }
+  }
+
+  if (!chosen) {
+    let x = map(mouseX, 0, width, -width/2, width/2);
+    let y = map(mouseY, 0, height, height/2, -height/2);
+    pos = createVector(x, y);
+    chosen = true;
+
+    noStroke();
+    fill(255);
+    ellipse(pos.x, pos.y, 4.5, 4.5);
+  }
+}
